9f407158cc997e331567483bf93565210d7f91fd
George <gheorghita.harpa0423@yahoo.com>
2020-06-10 00:11:28 +0100

    Implemented Post API.

---
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.PostRequest;
+import com.example.demo.dto.PostResponse;
+import com.example.demo.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts/")
+@AllArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<Void> createPost(@RequestBody PostRequest postRequest) {
+        postService.save(postRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<PostResponse> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public PostResponse getPost(@PathVariable Long id) {
+        return postService.getPost(id);
+    }
+
+    @GetMapping("/by-subreddit/{id}")
+    public List<PostResponse> getPostsBySubreddit(Long id) {
+        return postService.getPostsBySubreddit(id);
+    }
+
+    @GetMapping("/by-user/{name}")
+    public List<PostResponse> getPostsByUsername(String username) {
+        return postService.getPostsByUsername(username);
+    }
+}
--- src/main/java/com/example/demo/service/SubredditService.java
+++ src/main/java/com/example/demo/service/SubredditService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.SubredditDto;
+import com.example.demo.exceptions.SpringRedditException;
+import com.example.demo.mapper.SubredditMapper;
 import com.example.demo.model.Subreddit;
 import com.example.demo.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
@@ -18,10 +20,11 @@ import static java.util.stream.Collectors.toList;
 public class SubredditService {
 
     private final SubredditRepository subredditRepository;
+    private final SubredditMapper subredditMapper;
 
     @Transactional
     public SubredditDto save(SubredditDto subredditDto) {
-        Subreddit subreddit = mapSubredditDto(subredditDto);
+        Subreddit subreddit = subredditMapper.mapDtoToSubreddit(subredditDto);
         Subreddit save = subredditRepository.save(subreddit);
         subredditDto.setId(save.getId());
 
@@ -32,11 +35,18 @@ public class SubredditService {
     public List<SubredditDto> getAll() {
         return subredditRepository.findAll()
                 .stream()
-                .map(this::mapToDto)
+                .map(subredditMapper::mapSubredditToDto)
                 .collect(toList());
     }
 
-    private SubredditDto mapToDto(Subreddit subredditDto) {
+    public SubredditDto getSubreddit(Long id) {
+        Subreddit subreddit = subredditRepository.findById(id)
+                .orElseThrow(() -> new SpringRedditException("No subreddit found with ID - " + id));
+        return subredditMapper.mapSubredditToDto(subreddit);
+    }
+
+    // Removed and replaced with the methods in SubredditMapper
+    /*private SubredditDto mapToDto(Subreddit subredditDto) {
         return SubredditDto.builder().name(subredditDto.getName())
                 .description(subredditDto.getDescription())
                 .build();
@@ -47,5 +57,5 @@ public class SubredditService {
                 .description(subredditDto.getDescription())
                 .build();
 
-    }
+    }*/
 }
